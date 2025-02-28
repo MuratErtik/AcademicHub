e6e461bee5362fd8018373f926f9604ab88628a8
Murat Ertik <118116524+MuratErtik@users.noreply.github.com>
2025-02-28 08:53:39 -0800

    Create scripts.sql

---
+++ scripts.sql
@@ -0,0 +1,20 @@
+INSERT INTO faculty_group (faculty_group_name) 
+VALUES 
+    ('Sağlık Bilimleri, Fen
+Bilimleri ve
+Matematik,
+Mühendislik, Ziraat,
+Orman ve Su Ürünleri
+Temel Alanı'),
+    ('Eğitim Bilimleri,
+Filoloji, Mimarlık,
+Planlama ve Tasarım,
+Sosyal, Beşeri ve İdari
+Bilimler ile Spor
+Bilimleri
+Temel Alanı'),
+    ('Hukuk, İlahiyat
+Temel Alanı'),
+    ('Güzel Sanatlar
+(Konservatuvar dahil)
+Temel Alanı');
