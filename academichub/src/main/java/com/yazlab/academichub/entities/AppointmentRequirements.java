9f0122149e8d931a6b3a5f72b8aaecce10e49c73
Murat Ertik <dmuratertik1@gmail.com>
2025-03-08 11:56:57 -0800

    appointment requirements has been created.

---
+++ academichub/src/main/java/com/yazlab/academichub/entities/AppointmentRequirements.java
@@ -0,0 +1,41 @@
+package com.yazlab.academichub.entities;
+
+import com.yazlab.academichub.domain.POSITION;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class AppointmentRequirements {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long appointmentRequirementsId;
+
+    private POSITION position;
+
+    @ManyToOne
+    @JoinColumn(name = "facultygroup_id", nullable = false)
+    private FacultyGroup facultyGroup;
+
+    @ManyToOne
+    @JoinColumn(name = "prerequisites_id", nullable = false)
+    private Prerequisites prerequisites;
+
+    @ManyToOne
+    @JoinColumn(name = "publicationcriteria_id", nullable = false)
+    private PublicationCriteria publicationCriteria;
+
+    @ManyToOne
+    @JoinColumn(name = "minmaxpointcriteria_id", nullable = false)
+    private MinMaxPointCriteria minMaxPointCriteria; 
+}
--- academichub/src/main/java/com/yazlab/academichub/entities/MinMaxPointCriteria.java
+++ academichub/src/main/java/com/yazlab/academichub/entities/MinMaxPointCriteria.java
@@ -1,5 +1,28 @@
 package com.yazlab.academichub.entities;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
 public class MinMaxPointCriteria {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long minMaxPointCriteriaId;
+
+    @ManyToOne
+    @JoinColumn(name = "table3Action_id", nullable = false)
+    private Table3Action table3Action;
+
+    private int minPoint; 
+
+    private int maxPoint;
 }
