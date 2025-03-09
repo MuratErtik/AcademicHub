6acb9e449fda66a28a6bdc041732a35fdaab9312
Murat Ertik <dmuratertik1@gmail.com>
2025-03-09 14:01:32 -0700

    research project  part has been completed.

---
+++ academichub/src/main/java/com/yazlab/academichub/entities/CandidateResearchProject.java
@@ -0,0 +1,33 @@
+package com.yazlab.academichub.entities;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+public class CandidateResearchProject {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long candidateResearchProjectId;
+
+    private String category;
+
+    private String name;
+
+    private Long number;
+
+    private String year;
+
+    private String uniName;
+
+    private String photoLink;
+
+}
