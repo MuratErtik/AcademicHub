2a42fe73b99e6ca31b92ddf122e9b461ad097552
Murat Ertik <dmuratertik1@gmail.com>
2025-03-16 08:38:10 -0700

    secretkey and header has been created.

---
+++ academichub/src/main/java/com/yazlab/academichub/config/JWT_CONSTANT.java
@@ -0,0 +1,7 @@
+package com.yazlab.academichub.config;
+
+public class JWT_CONSTANT {
+    public static final String SECRET_KEY = "klvdsndnvlnvkfnkdnkfdkfdfkdnfdjknfknjgnfkfgnjdksweoimewiodklvnkldnvkdnvldknvkldnvldnkvldnvldnvdklnv";
+
+    public static final String JWT_HEADER = "Authorization";
+}
