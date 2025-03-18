9f72e93ccc906d630bd645d3dd0693ca77c01045
Murat Ertik <dmuratertik1@gmail.com>
2025-03-18 04:16:18 -0700

    Exception packages have been created.

---
+++ academichub/src/main/java/com/yazlab/academichub/exception/AuthException.java
@@ -0,0 +1,9 @@
+package com.yazlab.academichub.exception;
+
+public class AuthException extends Exception {
+
+    public AuthException(String message){
+        super(message);
+    }
+
+}
---
+++ academichub/src/main/java/com/yazlab/academichub/exception/GlobalException.java
@@ -0,0 +1,25 @@
+package com.yazlab.academichub.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class GlobalException {
+
+    @ExceptionHandler(AuthException.class)
+    public ResponseEntity<ErrorDetail> sellerExceptionHandler(AuthException ae, WebRequest webRequest) {
+
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setError(ae.getMessage());
+        errorDetail.setDetails(webRequest.getDescription(false));
+        errorDetail.setTimestamp(LocalDateTime.now());
+
+        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
+
+    }
+}
