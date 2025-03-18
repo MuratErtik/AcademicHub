b5132efbbb831cc032603d7b0e893e3aa6f41b46
Murat Ertik <dmuratertik1@gmail.com>
2025-03-18 10:01:45 -0700

    User Repository has been created.

---
+++ academichub/src/main/java/com/yazlab/academichub/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.yazlab.academichub.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.yazlab.academichub.entities.User;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    
+    User findByEmail(String email);
+}
