2db244242c0e950d2ad78a8f9c4d9b7fb9c7ed49
Murat Ertik <dmuratertik1@gmail.com>
2025-03-19 00:36:05 -0700

    signup request has been added for the other roles.

---
+++ academichub/src/main/java/com/yazlab/academichub/request/OtherSignupRequest.java
@@ -0,0 +1,20 @@
+package com.yazlab.academichub.request;
+
+import lombok.Data;
+
+@Data
+public class OtherSignupRequest {
+
+    private String email;
+
+    private String tcNo;
+
+    private String name;
+
+    private String lastname;
+
+    private Long mobileNo;
+
+    private String password;
+
+}
