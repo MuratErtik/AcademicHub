b5f38f43ba95ffabdb2d4ea4496c1632ddce2856
Murat Ertik <dmuratertik1@gmail.com>
2025-03-19 00:34:48 -0700

    admin auth has been added.

--- academichub/src/main/java/com/yazlab/academichub/controller/AuthController.java
+++ academichub/src/main/java/com/yazlab/academichub/controller/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.yazlab.academichub.domain.USER_ROLE;
 import com.yazlab.academichub.exception.AuthException;
+import com.yazlab.academichub.request.OtherSignupRequest;
 import com.yazlab.academichub.request.SignupRequest;
 import com.yazlab.academichub.response.AuthResponse;
 import com.yazlab.academichub.service.AuthService;
@@ -22,7 +23,7 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/signin")
-    public ResponseEntity<AuthResponse> loginHandler(@RequestBody SignupRequest signupRequest) throws AuthException{
+    public ResponseEntity<AuthResponse> loginHandler(@RequestBody SignupRequest signupRequest) throws AuthException {
 
         AuthResponse authResponse = authService.signIn(signupRequest);
 
@@ -30,10 +31,27 @@ public class AuthController {
 
     }
 
-    @PostMapping("signup")
-    public ResponseEntity<AuthResponse> createCandidateHandler(@RequestBody SignupRequest request){
+    // @PostMapping("signup")
+    // public ResponseEntity<AuthResponse> createCandidateHandler(@RequestBody
+    // SignupRequest request){
 
-        String jwt = authService.createCandidate(request);
+    // String jwt = authService.createCandidate(request);
+
+    // AuthResponse authResponse = new AuthResponse();
+
+    // authResponse.setJwt(jwt);
+
+    // authResponse.setMessage("Registered Successfully");
+
+    // authResponse.setRole(USER_ROLE.CANDIDATE);
+
+    // return ResponseEntity.ok(authResponse);
+    // }
+
+    @PostMapping("admin-signup")
+    public ResponseEntity<AuthResponse> createCandidateHandler(@RequestBody OtherSignupRequest request) {
+
+        String jwt = authService.createAdmin(request);
 
         AuthResponse authResponse = new AuthResponse();
 
@@ -41,10 +59,20 @@ public class AuthController {
 
         authResponse.setMessage("Registered Successfully");
 
-        authResponse.setRole(USER_ROLE.CANDIDATE);
+        authResponse.setRole(USER_ROLE.ADMIN);
 
         return ResponseEntity.ok(authResponse);
+
+
+    }
+
+    @PostMapping("/admin-signin")
+    public ResponseEntity<AuthResponse> adminLoginHandler(@RequestBody OtherSignupRequest signupRequest) throws AuthException {
+
+        AuthResponse authResponse = authService.adminSignIn(signupRequest);
+
+        return ResponseEntity.ok(authResponse);
+
     }
 
 }
-    
--- academichub/src/main/java/com/yazlab/academichub/service/AuthService.java
+++ academichub/src/main/java/com/yazlab/academichub/service/AuthService.java
@@ -16,6 +16,7 @@ import com.yazlab.academichub.domain.USER_ROLE;
 import com.yazlab.academichub.entities.User;
 import com.yazlab.academichub.exception.AuthException;
 import com.yazlab.academichub.repository.UserRepository;
+import com.yazlab.academichub.request.OtherSignupRequest;
 import com.yazlab.academichub.request.SignupRequest;
 import com.yazlab.academichub.response.AuthResponse;
 
@@ -31,9 +32,81 @@ public class AuthService {
 
     private final JwtProvider jwtProvider;
 
-    public String createCandidate(SignupRequest req) {
+    public User createCandidate(SignupRequest req) {
+
+        // User user = userRepository.findByEmail(req.getEmail());
+
+        User newUser = new User();
+
+        newUser.setEmail(req.getEmail());
+
+        newUser.setLastname(req.getLastname());
+
+        newUser.setMobileNo(req.getMobileNo());
+
+        newUser.setName(req.getName());
+
+        newUser.setTcNo(passwordEncoder.encode(req.getTcNo()));
+
+        newUser.setUserRole(USER_ROLE.CANDIDATE);
+
+        return userRepository.save(newUser);
+
+        // yeni nesneler olusturulabilir buradan -> basvuru vs...
+
+        // List<GrantedAuthority> authorities = new ArrayList<>();
+
+        // authorities.add(new SimpleGrantedAuthority(USER_ROLE.CANDIDATE.toString()));
+
+        // Authentication authentication = new
+        // UsernamePasswordAuthenticationToken(req.getEmail(), null, authorities);
+
+        // SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        // return jwtProvider.generateToken(authentication);
+
+
+    }
+
+    public AuthResponse signIn(SignupRequest req) throws AuthException {
 
         User user = userRepository.findByEmail(req.getEmail());
+
+        if (user == null) {
+            user = createCandidate(req);
+            //
+        }
+
+        if (!passwordEncoder.matches(req.getTcNo(), user.getTcNo())) {
+            throw new AuthException("Invalid Tc No!");
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority(user.getUserRole().toString()));
+
+        Authentication authentication = new UsernamePasswordAuthenticationToken(user.getEmail(), null, authorities);
+
+        String token = jwtProvider.generateToken(authentication);
+
+        String message = "login successfully!";
+
+        String roleName = authorities.isEmpty() ? null : authorities.iterator().next().getAuthority();
+
+        AuthResponse authResponse = new AuthResponse();
+
+        authResponse.setJwt(token);
+
+        authResponse.setMessage(message);
+
+        authResponse.setRole(USER_ROLE.valueOf(roleName));
+
+        return authResponse;
+
+    }
+
+    public String createAdmin(OtherSignupRequest req) {
+
+        User user = userRepository.findByEmail(req.getTcNo());
 
         if (user == null) {
 
@@ -49,7 +122,9 @@ public class AuthService {
 
             newUser.setTcNo(passwordEncoder.encode(req.getTcNo()));
 
-            newUser.setUserRole(USER_ROLE.CANDIDATE);
+            newUser.setPassword(passwordEncoder.encode(req.getPassword()));
+
+            newUser.setUserRole(USER_ROLE.ADMIN);
 
             user = userRepository.save(newUser);
 
@@ -59,7 +134,7 @@ public class AuthService {
 
         List<GrantedAuthority> authorities = new ArrayList<>();
 
-        authorities.add(new SimpleGrantedAuthority(USER_ROLE.CANDIDATE.toString()));
+        authorities.add(new SimpleGrantedAuthority(USER_ROLE.ADMIN.toString()));
 
         Authentication authentication = new UsernamePasswordAuthenticationToken(req.getEmail(), null, authorities);
 
@@ -69,16 +144,20 @@ public class AuthService {
 
     }
 
-    public AuthResponse signIn(SignupRequest req) throws AuthException {
+    public AuthResponse adminSignIn(OtherSignupRequest req) throws AuthException {
 
-        User user = userRepository.findByEmail(req.getEmail());
+        User user = userRepository.findByEmail(req.getEmail()); 
 
         if (user == null) {
-            createCandidate(req);
+            throw new AuthException("User has not been sign up before!");
         }
 
         if (!passwordEncoder.matches(req.getTcNo(), user.getTcNo())) {
             throw new AuthException("Invalid Tc No!");
+        }
+
+        if (!passwordEncoder.matches(req.getPassword(), user.getPassword())) {
+            throw new AuthException("Invalid password!");
         }
 
         List<GrantedAuthority> authorities = new ArrayList<>();
