ef6cb5a7eb7eacc3c5cc27d07d009ad17096ebd3
Murat Ertik <dmuratertik1@gmail.com>
2025-03-23 07:16:58 -0700

    Get User By Email Method has been added in it.

--- academichub/src/main/java/com/yazlab/academichub/controller/AdminController.java
+++ academichub/src/main/java/com/yazlab/academichub/controller/AdminController.java
@@ -3,11 +3,14 @@ package com.yazlab.academichub.controller;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yazlab.academichub.domain.USER_ROLE;
+import com.yazlab.academichub.entities.User;
+import com.yazlab.academichub.exception.AdminException;
 import com.yazlab.academichub.response.UserResponse;
 import com.yazlab.academichub.service.AdminService;
 
@@ -24,6 +27,12 @@ public class AdminController {
     public List<UserResponse> getUsersByRole(@RequestParam USER_ROLE userRole) {
 
         return adminService.getUsersByRole(userRole);
+    }
+
+    @GetMapping("/getadminbyemail")
+    public User getUserByEmail(@RequestBody String email) throws AdminException {
+
+        return adminService.getAdminByEmail(email);
     }
 
 }
