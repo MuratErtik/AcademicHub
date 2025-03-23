924832ccb4ea41c51b22fe6786cbd4acf512cc11
Murat Ertik <dmuratertik1@gmail.com>
2025-03-23 04:19:37 -0700

    AdninException has been completed.

--- academichub/src/main/java/com/yazlab/academichub/exception/GlobalException.java
+++ academichub/src/main/java/com/yazlab/academichub/exception/GlobalException.java
@@ -34,4 +34,16 @@ public class GlobalException {
         return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
 
     }
+
+    @ExceptionHandler(AdminException.class)
+    public ResponseEntity<ErrorDetail> AdminExceptionHandler(AdminException ae, WebRequest webRequest) {
+
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setError(ae.getMessage());
+        errorDetail.setDetails(webRequest.getDescription(false));
+        errorDetail.setTimestamp(LocalDateTime.now());
+
+        return new ResponseEntity<>(errorDetail, HttpStatus.BAD_REQUEST);
+
+    }
 }
