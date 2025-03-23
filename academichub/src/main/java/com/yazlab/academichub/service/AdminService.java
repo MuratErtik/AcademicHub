866d7140e773a4cf569f1ba593a40f2e233bef8b
Murat Ertik <dmuratertik1@gmail.com>
2025-03-23 07:17:29 -0700

    inner table has been created for job offer.

--- academichub/src/main/java/com/yazlab/academichub/service/AdminService.java
+++ academichub/src/main/java/com/yazlab/academichub/service/AdminService.java
@@ -1,13 +1,16 @@
 package com.yazlab.academichub.service;
 
-
 import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Service;
 
 import com.yazlab.academichub.domain.USER_ROLE;
+import com.yazlab.academichub.entities.DepartmentMenagerJobOffer;
+import com.yazlab.academichub.entities.JobOffer;
 import com.yazlab.academichub.entities.User;
+import com.yazlab.academichub.exception.AdminException;
+import com.yazlab.academichub.repository.DepartmentMenagerJobOfferRepository;
 import com.yazlab.academichub.repository.UserRepository;
 import com.yazlab.academichub.response.UserResponse;
 
@@ -19,26 +22,24 @@ import lombok.RequiredArgsConstructor;
 // paneline erişir.
 // 2. Yeni ilan ekleyebilir veya mevcut ilanları
 // duzenleyebilir.
-// 3. İ lanların başlangıç ve bitiş tarihleri, gerekli
+// 3. İlanların başlangıç ve bitiş tarihleri, gerekli
 // belgeler ve başvuru koşulları belirlenebilir.
-// 4. İ lan başvurularını go ru ntu leyebilir ve
-// gerektig inde yetkililere yo nlendirebilir.
+// 4. İlan başvurularını goruntuleyebilir ve
+// gerektiginde yetkililere yonlendirebilir.
 public class AdminService {
 
     private final UserRepository userRepository;
 
+    private final DepartmentMenagerJobOfferRepository departmentMenagerJobOfferRepository;
 
     public List<UserResponse> getUsersByRole(USER_ROLE role) {
 
         List<User> users = userRepository.findByUserRole(role);
 
         List<UserResponse> userResponses = users.stream()
-                                                .map(this::convertToUserResponse)
-                                                .collect(Collectors.toList());
+                .map(this::convertToUserResponse)
+                .collect(Collectors.toList());
 
-        
-
-        
         return userResponses;
     }
 
@@ -61,13 +62,50 @@ public class AdminService {
         return userResponse;
     }
 
+    // ilana yonetici eklemem lazim!
+    // ilan acildigi zaman yonetici de eklemek gerekir
+
+    public List<DepartmentMenagerJobOffer> addManegerToJobOffer(USER_ROLE role, Long departmentId, JobOffer jobOffer) {
+
+        List<User> usersToAddMenagerJobOffer = userRepository.findByUserRoleAndDepartment(role, departmentId);
+
+        List<DepartmentMenagerJobOffer> departmentMenagerJobOffers = usersToAddMenagerJobOffer.stream()
+                .map(user -> addToInnerTable(user, jobOffer))
+                .collect(Collectors.toList());
+
+        return departmentMenagerJobOffers;
+
+    }
+
+    public DepartmentMenagerJobOffer addToInnerTable(User user, JobOffer jobOffer) {
+
+        DepartmentMenagerJobOffer departmentMenagerJobOffer = new DepartmentMenagerJobOffer();
+
+        departmentMenagerJobOffer.setDepartmentMenager(user);
+
+        departmentMenagerJobOffer.setJobOffer(jobOffer);
+
+        return departmentMenagerJobOfferRepository.save(departmentMenagerJobOffer);
+    }
+
+    public User getAdminByEmail(String email) throws AdminException{
+
+        String cleanedEmail = email.trim().replace("\"", "");
+
+        User user = userRepository.findByEmail(cleanedEmail);
+
+        System.out.println("*********************************************");
+        System.out.println(email);
+        System.out.println("*********************************************");
+
+
         
 
+        if (user == null) {
+            throw new AdminException("Admin could not find with email -> " + cleanedEmail);
+        }
 
+        return user;
+    }
 
-
-
-
-
-  
 }
