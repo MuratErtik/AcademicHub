61200698976c7e6f9c89d4ea9705f83ee3c8ff96
Murat Ertik <dmuratertik1@gmail.com>
2025-03-23 07:11:44 -0700

    new queries have been added.

--- academichub/src/main/java/com/yazlab/academichub/repository/UserRepository.java
+++ academichub/src/main/java/com/yazlab/academichub/repository/UserRepository.java
@@ -3,8 +3,11 @@ package com.yazlab.academichub.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.yazlab.academichub.domain.USER_ROLE;
+import com.yazlab.academichub.entities.Department;
 import com.yazlab.academichub.entities.User;
 
 public interface UserRepository extends JpaRepository<User,Long> {
@@ -14,4 +17,9 @@ public interface UserRepository extends JpaRepository<User,Long> {
     User findByTcNo(String tcNo);
 
     List<User>  findByUserRole(USER_ROLE role);
+
+    List<User> findByDepartment(Department department);
+
+    @Query(value = "select * from users where user_role = :role and department_id = :departmentId", nativeQuery = true)
+    List<User>  findByUserRoleAndDepartment(@Param("role") USER_ROLE role, @Param("departmentId") Long departmentId);
 }
