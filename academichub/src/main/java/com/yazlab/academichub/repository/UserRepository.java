93191eb306a7ca1faa5a6342c27f7afbda147477
Murat Ertik <dmuratertik1@gmail.com>
2025-03-25 06:40:47 -0700

    Query parameter errors have been solved.

--- academichub/src/main/java/com/yazlab/academichub/repository/UserRepository.java
+++ academichub/src/main/java/com/yazlab/academichub/repository/UserRepository.java
@@ -6,9 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-import com.yazlab.academichub.domain.USER_ROLE;
 import com.yazlab.academichub.entities.Department;
 import com.yazlab.academichub.entities.User;
+import com.yazlab.academichub.entities.UserRole;
 
 public interface UserRepository extends JpaRepository<User,Long> {
     
@@ -16,10 +16,10 @@ public interface UserRepository extends JpaRepository<User,Long> {
 
     User findByTcNo(String tcNo);
 
-    List<User>  findByUserRole(USER_ROLE role);
+    List<User>  findByUserRole(UserRole userRole);
 
     List<User> findByDepartment(Department department);
 
-    @Query(value = "select * from users where user_role = :role and department_id = :departmentId", nativeQuery = true)
-    List<User>  findByUserRoleAndDepartment(@Param("role") USER_ROLE role, @Param("departmentId") Long departmentId);
+    @Query(value = "select * from users where role_id = :roleId and department_id = :departmentId", nativeQuery = true)
+    List<User>  findByUserRoleAndDepartment(@Param("roleId") Long roleId, @Param("departmentId") Long departmentId);
 }
