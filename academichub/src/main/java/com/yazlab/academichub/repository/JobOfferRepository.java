f723d45e1c2ae27ccc3f2c7981897a4e23e27f1e
Murat Ertik <dmuratertik1@gmail.com>
2025-03-26 11:53:25 -0700

    findById has been updated.

--- academichub/src/main/java/com/yazlab/academichub/repository/JobOfferRepository.java
+++ academichub/src/main/java/com/yazlab/academichub/repository/JobOfferRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.yazlab.academichub.entities.JobOffer;
 
 public interface JobOfferRepository extends JpaRepository<JobOffer,Long>{
-    
+    JobOffer findByJobOfferId(Long jobOfferId);
 }
