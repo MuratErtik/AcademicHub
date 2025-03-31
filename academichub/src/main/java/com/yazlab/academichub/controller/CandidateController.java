9a23f3c9e7dec7fb56aebec9e971af401a9fb07e
Murat Ertik <dmuratertik1@gmail.com>
2025-03-31 13:12:49 -0700

    Start Application feature has been completed.

--- academichub/src/main/java/com/yazlab/academichub/controller/CandidateController.java
+++ academichub/src/main/java/com/yazlab/academichub/controller/CandidateController.java
@@ -5,12 +5,18 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.yazlab.academichub.config.JwtProvider;
+import com.yazlab.academichub.entities.User;
 import com.yazlab.academichub.exception.AdminException;
+import com.yazlab.academichub.exception.JobOfferException;
+import com.yazlab.academichub.repository.UserRepository;
+import com.yazlab.academichub.response.ApiResponse;
 import com.yazlab.academichub.response.JobOfferResponse;
 import com.yazlab.academichub.service.CandidateService;
 
@@ -25,6 +31,8 @@ public class CandidateController {
 
     private final JwtProvider jwtProvider;
 
+    private final UserRepository userRepository;
+
     @GetMapping("/getAllJobOffer")
     public ResponseEntity<List<JobOfferResponse>> getAJobOffers(@RequestHeader("Authorization") String jwt)
             throws AdminException {
@@ -36,9 +44,31 @@ public class CandidateController {
             List<JobOfferResponse> jobOffers = candidateService.listAllOffers();
 
             return ResponseEntity.ok(jobOffers);
+        } else {
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+        }
+
+    }
+
+    @PostMapping("/{jobOfferId}/start-application")
+    public ResponseEntity<ApiResponse> createApplication(@RequestHeader("Authorization") String jwt,
+            @PathVariable Long jobOfferId) throws AdminException, JobOfferException {
+
+        String email = jwtProvider.getEmailFromJwtToken(jwt);
+
+        String role = jwtProvider.getRolefromjwtByEmail(email);
+
+        User user = userRepository.findByEmail(email);
+
+        Long userId = (Long)user.getUserId();
+
+        if (role.equals("ADAY")) {
+            ApiResponse apiResponse = candidateService.createApplication(jobOfferId, userId);
+
+            return new ResponseEntity<>(apiResponse, HttpStatus.CREATED);
         }
         else{
-            return  new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
         }
 
     }
---
+++ academichub/src/main/java/com/yazlab/academichub/service/CandidateArticleService.java
@@ -0,0 +1,28 @@
+package com.yazlab.academichub.service;
+
+import org.springframework.stereotype.Service;
+
+import com.yazlab.academichub.response.ApiResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class CandidateArticleService {
+
+    //add new 
+    //delete
+    //update
+
+    public ApiResponse getOneArticle(){
+
+        
+
+
+
+
+        return null;
+    }
+
+
+}
