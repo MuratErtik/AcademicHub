949dffc10f23d864710c7850c872c7396b6f6e0d
Murat Ertik <dmuratertik1@gmail.com>
2025-04-01 03:43:52 -0700

    Extends from JPA Repository.

--- academichub/src/main/java/com/yazlab/academichub/repository/AuthorTypeRepository.java
+++ academichub/src/main/java/com/yazlab/academichub/repository/AuthorTypeRepository.java
@@ -1,8 +1,10 @@
 package com.yazlab.academichub.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import com.yazlab.academichub.entities.AuthorType;
 
-public interface AuthorTypeRepository {
+public interface AuthorTypeRepository extends  JpaRepository<AuthorType,Long> {
 
     AuthorType findByAuthorTypeName(String name);
 }
