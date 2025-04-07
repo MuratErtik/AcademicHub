1f04c660b63c6871621fe37ac4d188e80b659b6b
Murat Ertik <dmuratertik1@gmail.com>
2025-04-07 10:59:54 -0700

    JsonIgnore has been removed.

--- academichub/src/main/java/com/yazlab/academichub/entities/JuryApplication.java
+++ academichub/src/main/java/com/yazlab/academichub/entities/JuryApplication.java
@@ -1,6 +1,5 @@
 package com.yazlab.academichub.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.*;
 import lombok.Data;
@@ -17,7 +16,6 @@ public class JuryApplication {
     @JoinColumn(name = "user_id", nullable = false) 
     private User jury;
 
-    @JsonIgnore
     @ManyToOne
     @JoinColumn(name = "application_id",nullable = false)
     private Application application;
